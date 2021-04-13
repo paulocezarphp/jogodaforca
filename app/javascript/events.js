0a57e6230e04002b3046adc78c61d959af35c3b9
Cezar Leigos <cezarleigos28@outlook.com>
2021-04-12 21:01:03 -0300

    v0.5 - Colocando Botões
    Dessa vez nós adicionamos os botões para que possamos acionar os eventos da escolha do usuário.

--- app/javascript/events.js
+++ app/javascript/events.js
@@ -1,3 +1,107 @@
 document.getElementById("bot_a").addEventListener("click", function() {
-    
+   alert("A"); 
 });
+
+document.getElementById("bot_b").addEventListener("click", function() {
+   alert("B"); 
+});
+
+document.getElementById("bot_c").addEventListener("click", function() {
+   alert("C"); 
+});
+
+document.getElementById("bot_d").addEventListener("click", function() {
+   alert("D"); 
+});
+
+document.getElementById("bot_e").addEventListener("click", function() {
+   alert("E"); 
+});
+
+document.getElementById("bot_f").addEventListener("click", function() {
+   alert("F"); 
+});
+
+document.getElementById("bot_g").addEventListener("click", function() {
+   alert("G"); 
+});
+
+document.getElementById("bot_h").addEventListener("click", function() {
+   alert("H"); 
+});
+
+document.getElementById("bot_i").addEventListener("click", function() {
+   alert("I"); 
+});
+
+document.getElementById("bot_j").addEventListener("click", function() {
+   alert("J"); 
+});
+
+document.getElementById("bot_l").addEventListener("click", function() {
+   alert("L"); 
+});
+
+document.getElementById("bot_m").addEventListener("click", function() {
+   alert("M"); 
+});
+
+document.getElementById("bot_n").addEventListener("click", function() {
+   alert("N"); 
+});
+
+document.getElementById("bot_o").addEventListener("click", function() {
+   alert("O"); 
+});
+
+document.getElementById("bot_p").addEventListener("click", function() {
+   alert("P"); 
+});
+
+document.getElementById("bot_q").addEventListener("click", function() {
+   alert("Q"); 
+});
+
+document.getElementById("bot_r").addEventListener("click", function() {
+   alert("R"); 
+});
+
+document.getElementById("bot_s").addEventListener("click", function() {
+   alert("S"); 
+});
+
+document.getElementById("bot_t").addEventListener("click", function() {
+   alert("T"); 
+});
+
+document.getElementById("bot_u").addEventListener("click", function() {
+   alert("U"); 
+});
+
+document.getElementById("bot_v").addEventListener("click", function() {
+   alert("V"); 
+});
+
+document.getElementById("bot_x").addEventListener("click", function() {
+   alert("X"); 
+});
+
+document.getElementById("bot_z").addEventListener("click", function() {
+   alert("Z"); 
+});
+
+document.getElementById("bot_w").addEventListener("click", function() {
+   alert("W"); 
+});
+
+document.getElementById("bot_y").addEventListener("click", function() {
+   alert("Y"); 
+});
+
+document.getElementById("bot_k").addEventListener("click", function() {
+   alert("K"); 
+});
+
+
+
+
